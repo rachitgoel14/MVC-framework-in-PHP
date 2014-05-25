478b8c53fc85e4fc50366f6d251248d870e1f4d0
Rachit Goel <rachitgoel.jiit14@gmail.com>
2014-05-25 13:36:49 +0530

    MVC directory tree created

---
+++ app/core/App.php
@@ -0,0 +1,48 @@
+<?php
+
+class App{
+    
+    protected $controller = 'home';
+    protected $home = 'index';
+    protected $params = [];
+    
+    public function __construct()
+    {
+        //print_r($this->parseUrl());
+        $url = $this->parseUrl();
+        
+        print_r($url);
+        
+        if(file_exists('..app/controllers/' . $url[0] . '.php'))
+        {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+        
+        require_once '../app/controllers/' . $this->controller . '.php';
+        
+        //echo $this->controller;
+        $this->controller = new $this->controller;
+        
+        //var_dump($this->controller);
+        
+        if(isset($url[1]))
+        {
+            if(method_exists($this->controller, $url[1]))
+            {
+                
+            }
+        }
+        
+    }
+    
+    public function parseUrl()
+    {
+        if(isset($_GET['url']))
+        {
+            //echo $_GET['url'];
+            return $url =                     explode('/',filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
+            
+        }
+    }
+}
